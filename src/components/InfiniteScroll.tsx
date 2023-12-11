28f66b00a88b57c805eecf5befa288eeb820ed60
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2023-12-12 03:36:02 +0800

    extracted infinite scroll as its own component

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -1,11 +1,64 @@
 "use client";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useRef, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useObserver } from "@/hooks";
 import PostSkeleton from "./posts/PostSkeleton";
 
-export default function InfiniteScroll({ nextKey }: { nextKey: number }) {
+export interface ItemList<T> {
+  items: T[];
+  key: number;
+  isLast: boolean;
+  setItems: React.Dispatch<React.SetStateAction<T[]>>;
+}
+
+interface InfiniteScrollProps<T> {
+  itemList: ItemList<T>;
+  children: React.ReactNode;
+}
+
+export default function InfiniteScroll<T>({
+  itemList,
+  children,
+}: InfiniteScrollProps<T>) {
   const router = useRouter();
+  const keyRef = useRef<number>(itemList.key);
+
+  useEffect(() => {
+    if (itemList.key === keyRef.current) {
+      return;
+    }
+
+    if (itemList.key > keyRef.current) {
+      keyRef.current = itemList.key;
+      itemList.setItems(itemList.items);
+      return;
+    }
+
+    keyRef.current = itemList.key;
+    itemList.setItems((prevItems: typeof itemList.items) => [
+      ...prevItems,
+      ...itemList.items,
+    ]);
+  }, [itemList, router]);
+
+  return (
+    <>
+      {children}
+      {!itemList.isLast && (
+        <SkeletonObserver router={router} nextKey={itemList.key} />
+      )}
+    </>
+  );
+}
+
+function SkeletonObserver({
+  nextKey,
+  router,
+}: {
+  nextKey: number;
+  router: AppRouterInstance;
+}) {
   const searchParams = useSearchParams();
   const currentKey = Number(searchParams.get("key"));
 
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,9 +1,8 @@
 "use client";
 import type { Post as TPost } from "@/lib/db/models";
 import type { PostList } from "@/services/posts";
-import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation";
-import InfiniteScroll from "../InfiniteScroll";
+import { useMemo, useState } from "react";
+import InfiniteScroll, { type ItemList } from "../InfiniteScroll";
 import Post from "./Post";
 import styles from "./Posts.module.css";
 
@@ -14,31 +13,23 @@ interface PostsProps {
 export default function Posts({ postList }: PostsProps) {
   const [posts, setPosts] = useState<TPost[]>(postList.posts);
 
-  const router = useRouter();
-  const keyRef = useRef<number>(postList.key);
-
-  useEffect(() => {
-    if (postList.key === keyRef.current) {
-      return;
-    }
-
-    if (postList.key > keyRef.current) {
-      keyRef.current = postList.key;
-      setPosts(postList.posts);
-      return;
-    }
-
-    keyRef.current = postList.key;
-    setPosts((prevPosts) => [...prevPosts, ...postList.posts]);
-  }, [postList, router]);
+  const itemList: ItemList<TPost> = useMemo(
+    () => ({
+      items: postList.posts,
+      key: postList.key,
+      isLast: postList.isLast,
+      setItems: setPosts,
+    }),
+    [postList]
+  );
 
   return (
     <div className={styles.posts}>
-      {posts?.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
-
-      {!postList.isLast && <InfiniteScroll nextKey={postList.key} />}
+      <InfiniteScroll itemList={itemList}>
+        {posts?.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
